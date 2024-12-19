47d88fd002e0f4d48d9ae62bcdd2d97dd0a5ee0f
lincw <56734434+quul@users.noreply.github.com>
2024-12-19 21:21:31 +0800

    fix: add diagram to command hint

--- src/scripts/setCommands.ts
+++ src/scripts/setCommands.ts
@@ -4,6 +4,7 @@ export const setCommands = async (bot: Bot) => {
   await bot.api.setMyCommands([
     { command: "start", description: "Start the bot" },
     { command: "temperature", description: "Get the temperature" },
+    { command: "diagram", description: "Get the diagram" },
     { command: "ping", description: "Ping the bot" },
   ])
 }
